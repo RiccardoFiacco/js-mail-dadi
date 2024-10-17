a5837adf94d511d196d1972b9a47ae62ed78e923
RiccardoFiacco <riccardofiacco17@gmail.com>
2024-10-17 14:21:55 +0200

    add  the two random numbers

---
+++ dadi/script.js
@@ -0,0 +1,7 @@
+// Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+// Stabilire il vincitore, in base a chi fa il punteggio più alto.
+
+
+let dadoPc = Math.floor(Math.random()*6);
+let dadoPlayer = Math.floor(Math.random()*6);
+console.log(dadoPc, dadoPlayer);
