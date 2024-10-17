2a4ac2a445c599fba940709c15219d040ba49690
RiccardoFiacco <riccardofiacco17@gmail.com>
2024-10-17 14:30:47 +0200

    add loop to make always a winner

--- dadi/script.js
+++ dadi/script.js
@@ -2,13 +2,28 @@
 // Stabilire il vincitore, in base a chi fa il punteggio più alto.
 
 
-let dadoPc = Math.floor(Math.random()*6);
-let dadoPlayer = Math.floor(Math.random()*6);
-console.log(dadoPc, dadoPlayer);
-if (dadoPc==dadoPlayer){
-    console.log("Pareggio, ripete!");
-}else if(dadoPc>dadoPlayer){
-    console.log("Vince il Pc");
-}else{
-    console.log("Vince il Player");
-}
+let dadoPc;
+let dadoPlayer;
+let ceVincitore = false;
+
+do{ 
+    dadoPc = Math.floor(Math.random()*6);
+    dadoPlayer = Math.floor(Math.random()*6);
+    console.log(dadoPc, dadoPlayer);
+
+    if (dadoPc==dadoPlayer){
+
+        console.log("Pareggio, ripete!");
+
+    }else if(dadoPc>dadoPlayer){
+
+        console.log("Vince il Pc");
+        ceVincitore = true;
+
+    }else{
+
+        console.log("Vince il Player");
+        ceVincitore = true;
+
+    }
+}while(!ceVincitore);
